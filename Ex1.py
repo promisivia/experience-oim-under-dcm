e70f6e6e7f3f0ffa1f48942566457e105b8f8314
FangnuoWu <fangnuowu@gmail.com>
2021-01-12 23:27:05 +0800

    feat: complete version of exe1

--- Ex1.py
+++ Ex1.py
@@ -5,20 +5,20 @@ from RunTools.MainForDCUCB import MainForDCUCB
 
 seed_size = 2
 iterations = 10000
-dataset_list = ['Small1']
-scale_list = ['0.2']
+dataset_list = ['Small1', 'Small2']
+scale_list = ['0.2', '0.5', '0.8', '0.3-0.7', '0.1-0.5', '0.5-0.9']
 
 if __name__ == '__main__':
-    for i in range(20):
+    for i in range(10):
         for dataset in dataset_list:
             for scale in scale_list:
-                save_address = 'SimulationResults/Ex1/' + dataset + '_' + scale + '_' + str(iterations) + '/'
+                save_address = 'SimulationResults/Ex1/' + dataset + '_' + scale + '/'
 
                 G = pickle.load(open('datasets/' + dataset + '/graph.G', 'rb'), encoding='latin1')
                 indegree = pickle.load(open('datasets/' + dataset + '/indegree.dic', 'rb'), encoding='latin1')
                 P = pickle.load(open('datasets/' + dataset + '/' + scale + '-Probability.dic', 'rb'), encoding='latin1')
                 parameter = pickle.load(open('datasets/' + dataset + '/nodeFeatures.dic', 'rb'), encoding='latin1')
-                feature_dic = pickle.load(open('datasets/' + dataset + '/edgeFeatures.dic', 'rb'), encoding='latin1')
+                feature_dic = pickle.load(open('datasets/' + dataset + '/random-edgeFeatures.dic', 'rb'), encoding='latin1')
 
                 MainForDC(G, indegree, P, parameter, feature_dic, iterations, seed_size, dataset, save_address, algorithm='UCB', times=1)
                 MainForCMAB(G, indegree, P, parameter, feature_dic, iterations, seed_size, dataset, save_address, times=1)
